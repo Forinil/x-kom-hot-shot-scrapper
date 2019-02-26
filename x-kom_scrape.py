e7ebc83c2647190be1d594ad94c937946e8cf014
Konrad Botor <kbotor@gmail.com>
2019-02-26 22:00:53 +0100

    Added URL and formatting to Slack message

--- x-kom_scrape.py
+++ x-kom_scrape.py
@@ -4,8 +4,9 @@ from slackclient import SlackClient
 from string import Template
 from urllib.request import urlopen
 
-import os
 import csv
+import os
+import re
 
 # Open page
 url = 'https://www.x-kom.pl/'
@@ -18,6 +19,9 @@ soup = BeautifulSoup(page, 'html.parser')
 product_name = soup.find('p', {'class': 'product-name'}).text.strip()
 old_price = soup.find('div', {'class': 'old-price'}).text.strip()
 new_price = soup.find('div', {'class': 'new-price'}).text.strip()
+hot_shot_script = soup.find_all('script', {'type': 'text/javascript'})[1].text.strip()
+regexp = re.compile(r"/goracy_strzal/\d+")
+hot_shot_url = regexp.search(hot_shot_script).group(0)
 
 # Print result to STDOUT
 print(product_name, old_price, new_price, sep=';')
@@ -32,8 +36,8 @@ slack_token = os.environ['SLACK_API_KEY']
 sc = SlackClient(slack_token)
 
 # Compose message
-message_template = Template('$product_name is now on sale. Old price: $old_price. New price: $new_price.')
-message_text = message_template.substitute(product_name=product_name, old_price=old_price, new_price=new_price)
+message_template = Template('><https://www.x-kom.pl$hot_shot_url|*$product_name*> is now on sale.\n>Old price: $old_price.\n>New price: $new_price.')
+message_text = message_template.substitute(hot_shot_url=hot_shot_url, product_name=product_name, old_price=old_price, new_price=new_price)
 
 # Send message
 sc.api_call(
